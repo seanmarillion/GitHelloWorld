e3c3b5d2cffb561f6ab2c34dd19b41f32948668a
seanmarillion <seanmarillion@gmail.com>
2010-05-07 10:12:17 -0600

    Made a change, added a String variable.

--- GitHelloWorld.java
+++ GitHelloWorld.java
@@ -2,6 +2,8 @@ class GitHelloWorld
 {
   public static void main(String [] args)
   {
+    String me = "Sean";
     System.out.println("Hello, Again, And again! GitWorld!");
+    System.out.println(me);
   }
 }
